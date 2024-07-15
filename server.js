b4580e37589d122d988364470d19eac90603ddf5
rjsander <rjsander19@gmail.com>
2024-07-14 19:42:34 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require("express")
+const app = express()
+
+const fruits = require("./models/fruits.js")
+
+app.get("/fruits/", (req, res) => {
+  res.send(fruits)
+})
+
+app.get("/fruits/:indexOfFruitsArray", (req, res) => {
+  res.send(fruits[req.params.indexOfFruitsArray])
+})
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("listening")
+})
